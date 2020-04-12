01080247d248118abfc6df6e6f8773bd0c1d1fcc
AdriSolid <biotekgis@gmail.com>
2020-04-12 20:22:22 +0200

    Updates

---
+++ precache-manifest.d6dffc64c2228b4a4fc25247553b4858.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "1bf118c2f59f091454d4",
+    "url": "/Void-techniques/static/css/main.82fa0acb.chunk.css"
+  },
+  {
+    "revision": "1bf118c2f59f091454d4",
+    "url": "/Void-techniques/static/js/main.e56b94a6.chunk.js"
+  },
+  {
+    "revision": "b2a505ad69d076405004",
+    "url": "/Void-techniques/static/js/runtime~main.508dec03.js"
+  },
+  {
+    "revision": "66f2e0060289b034a7d3",
+    "url": "/Void-techniques/static/js/2.4005688b.chunk.js"
+  },
+  {
+    "revision": "fc43c9a84a9851b5135701eaa59a776d",
+    "url": "/Void-techniques/index.html"
+  }
+];
